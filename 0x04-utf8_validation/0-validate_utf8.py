f6bd3ceb1406fa51b10e0e90640f6be14d9e32fc
Nwafor Ugochukwu Emmanuel <nwaforugochukwu21@gmail.com>
2024-04-29 20:45:15 +0000

    hot fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,30 +1,48 @@
 #!/usr/bin/python3
-"""
-UTF-8 Validation
-"""
+"""validUTF8 function takes in a list of integers and returns True"""
 
 
-
-def count_leads(byte):
-    """ counts the leading bytes in the data
+def validUTF8(data):  # type (list[int]) -> bool
     """
-    for i in range(8):
-        if byte >> (7 - i) == 0b11111111 >> (7 - i) & ~1:
-            return i
-    return 8
+    The validUTF8 function takes in a list of integers and returns True if
+    the list represents a valid UTF-8 encoding, or False otherwise.
 
-def validUTF8(data):
+    data `List[int]`: Store the data that is being passed into the function
+    return: True if the data is a valid utf-8 encoding
     """
-    method that determines if a given data set
-    represents a valid UTF-8 encoding
-    """
-    data = iter(data)
-    for leading_byte in data:
-        leading_ones = count_leads(leading_byte)
-        if leading_ones in [1, 7, 8]:
+    data_len = len(data)  # get the length of the dataset
+    i = 0  # initialize counter to 0
+    while i < data_len:  # loop thru all elements in dataset
+        num = data[i]
+        num_binary = '{0:08b}'.format(num)
+
+        # calculate the number of 1's
+        num_chars = 0
+        for char in num_binary:
+            if char == '1':
+                num_chars += 1
+                continue
+            break
+
+        if num_chars == 1 or num_chars > 4:
+            # 10XXX condition doesn't exist
+            # only 1 to 4 bytes long
             return False
-        for _ in range(leading_ones - 1):
-            trailing_byte = next(data, None)
-            if trailing_byte is None or trailing_byte >> 6 != 0b10:
+
+            # for 1 byte, num chars can be zero
+        num_chars = num_chars - 1 if num_chars > 1 else 0
+
+        check_till = i + num_chars
+        if check_till >= data_len:
+            # if there are not many required elements to check
+            return False
+
+        for j in range(i + 1, check_till + 1):
+            curr_num = data[j]
+            curr_num_binary = '{0:08b}'.format(curr_num)
+            if curr_num_binary[:2] != '10':
                 return False
+
+        i = check_till + 1
+
     return True
