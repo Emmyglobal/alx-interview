d1d3118c95eafd49517820e68f26c7caeceb3de5
Emmyglobal <nwaforugochukwu21@gmail.com>
2024-06-28 08:25:34 +0000

    Fixed

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,38 +9,38 @@ def island_perimeter(grid):
 
     perimeter = 0
     # Loops through the grid cell by cell.
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+    for n in range(len(grid)):
+        for m in range(len(grid[n])):
             # If the cell is not 0.
-            if grid[i][j]:
+            if grid[n][m]:
                 # Increments the perimeter by counting
                 # the water around the cell
-                perimeter += num_water(grid, i, j)
+                perimeter += num_water(grid, n, m)
 
     return perimeter
 
 
-def num_water(grid, i, j):
+def num_water(grid, n, m):
     """Returns the number of water a cell has in a grid."""
 
     # num increases if water is detected.
     num = 0
     # First Condition to check water on the cell's top side.
-    if i <= 0 or not grid[i - 1][j]:
+    if n <= 0 or not grid[n - 1][m]:
         num += 1
         # print(f"first if, {i},{j}")
     # Second Condition to check water on the cell's left side.
-    if j <= 0 or not grid[i][j - 1]:
+    if m <= 0 or not grid[n][m - 1]:
         num += 1
         # print(f"second if, {i},{j}")
     # Third Condition checks water on the cell's left side
     # and the if it's at the end grid.
 
-    if j >= len(grid[i]) - 1 or not grid[i][j - 1]:
+    if m >= len(grid[n]) - 1 or not grid[n][m - 1]:
         num += 1
         # print(f"third if, {i},{j}")
     # Fourth Condition checks water on the cell's down side.
-    if i >= len(grid) - 1 or not grid[i + 1][j]:
+    if n >= len(grid) - 1 or not grid[n + 1][m]:
         num += 1
         # print(f"fourth if, {i},{j}")
 
